364ff7d854a175cce6eb4da61b94f2e1eab238ea
Germano Nottingham <germanottingham@hotmail.com>
2021-10-28 15:42:05 -0300

    projeto atualizado

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-var express = require('express') //importa o módulo express neste arquivo
-var app = express() //iniciando o express
+var express = require('express')
+var app = express()
 var path = require('path')
 
-//criando a rota inicial
 app.get('/', function (req, res) {
-  res.sendFile(path.join(''))
-})
-
-//rota do cadastro de produtos
-app.get('/produtos', function (req, res) {
-  res.send('<h1>Lista de Produtos!</h1>')
-})
-app.get('/index', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/abnt', function (req, res) {
+  res.sendFile(__dirname + '/abnt.html')
+})
+
+app.get('/rota3', function (req, res) {
+  res.send('<h1>ROTA 3</h1>')
+})
+
 app.listen(4000, function (erro) {
-  // cria a aplicação na porta 4000
   if (erro) {
     console.log('Erro ao Iniciar.')
   } else {
